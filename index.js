3558b7540dbfaf22bc4d1f3782dd046dd17114fb
unknown <seftamilariani48@gmail.com>
2023-08-16 10:36:04 +0700

    upload source code

---
+++ index.js
@@ -0,0 +1,8 @@
+function KelilingSegitiga(){
+    let pertama=document.getElementById("sisi-a").value;
+    let kedua=document.getElementById("sisi-b").value;
+    let ketiga=document.getElementById("sisi-a").value;
+
+    let hasil=perseInt(pertama) + perseInt(kedua) + perseInt(ketiga);
+    console.log(hasil)
+}
